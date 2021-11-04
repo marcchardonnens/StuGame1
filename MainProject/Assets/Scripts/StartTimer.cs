408d7f6f82295f2b794fa1c85d532425f37687f4
hon3ypi <wanda.lao@gmail.com>
2021-11-04 21:13:50 +0100

    StartScene Timer

---
+++ MainProject/Assets/Scripts/StartTimer.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartTimer : MonoBehaviour
+{
+    private float timer;
+    void Start ()
+    {
+        timer = 10;
+    }
+ 
+    void Update ()
+    {
+        if( timer > 0 )
+        {
+            timer -= Time.deltaTime;
+
+            if( timer <= 0 )
+            {
+                SceneManager.LoadScene ("MenuScene");
+            }
+        }
+    }
+}

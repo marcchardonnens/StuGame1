d2cabfa080586cb2d80f1ab6ed9e1aca756c99f6
surfer412 <marcchardonnens@gmx.ch>
2021-11-25 02:16:39 +0100

    SecondsToMillis

---
+++ MainProject/Assets/Scripts/Util.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Util
+{
+    public static int SecondsToMillis(float s)
+    {
+        return (int)(s * 1000);
+    }
+}
